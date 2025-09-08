f667e85b71b1273c41bd8224d42a11caefe58ea3
crypto-2025 <cryptomarketingagency25@gmail.com>
2025-09-08 16:06:08 +0300

    update changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 ||3001;
 
 async function start() {
   try {
