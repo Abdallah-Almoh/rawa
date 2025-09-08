ae308d8275334a7adeb90e782652ba6d9608f2d3
crypto-2025 <cryptomarketingagency25@gmail.com>
2025-09-08 16:17:46 +0300

    update changes

--- swagger.js
+++ swagger.js
@@ -1,3 +1,31 @@
+// 'use strict';
+
+// const swaggerJsdoc = require('swagger-jsdoc');
+// const swaggerUi = require('swagger-ui-express');
+
+// const options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Rawaa API',
+//       version: '1.0.0',
+//       description: 'API documentation for Rawa project in port 3000',
+//     },
+//     servers: [
+//       { url: 'http://localhost:3000', description: 'Local server' }
+//     ],
+//   },
+//   apis: ['./routes/*.js', './controllers/*.js'], 
+// };
+
+// const swaggerSpec = swaggerJsdoc(options);
+
+// function setupSwagger(app) {
+//   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// }
+
+// module.exports = setupSwagger;
+
 'use strict';
 
 const swaggerJsdoc = require('swagger-jsdoc');
@@ -9,10 +37,17 @@ const options = {
     info: {
       title: 'Rawaa API',
       version: '1.0.0',
-      description: 'API documentation for Rawa project in port 3000',
+      description: 'API documentation for Rawaa project',
     },
     servers: [
-      { url: 'http://localhost:3000', description: 'Local server' }
+      {
+        url: 'http://tacticjo.ashyaaaonline.com:3001',
+        description: 'Production server',
+      },
+      {
+        url: 'http://localhost:3001',
+        description: 'Local development server',
+      },
     ],
   },
   apis: ['./routes/*.js', './controllers/*.js'], 
